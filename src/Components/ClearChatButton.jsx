57e8e1fe0b4de869ac7f799300c5198bf760ec73
tripathi_03 <mani03divy@gmail.com>
2023-05-05 16:10:02 +0530

    Chat app with delete button

---
+++ src/Components/ClearChatButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ClearChatButton({ onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clear Chat
+    </button>
+  );
+}
+
+export default ClearChatButton;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBBHA9z24i6xp4rV_-byW8jr7S3EHrCJs",
+  authDomain: "chatapp-9e44a.firebaseapp.com",
+  projectId: "chatapp-9e44a",
+  storageBucket: "chatapp-9e44a.appspot.com",
+  messagingSenderId: "789185447754",
+  appId: "1:789185447754:web:142c55082c494319ee23b3"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
